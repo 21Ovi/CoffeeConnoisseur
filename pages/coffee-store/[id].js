1d72b8f5a6eb08835fdc18530730c35851ec3ff7
= <=>
2022-06-14 12:11:18 +0530

    latest

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,9 +1,20 @@
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 const CoffeeStore = () => {
   const router = useRouter();
   console.log("router", router);
-  return <div>Coffee Store Page{router.query.id}</div>;
+  return (
+    <div>
+      Coffee Store Page{router.query.id}
+      <Link href="/">
+        <a>Back to home</a>
+      </Link>
+      <Link href="/coffee-store/one">
+        <a>Go to page Dynamic</a>
+      </Link>
+    </div>
+  );
 };
 
 export default CoffeeStore;
