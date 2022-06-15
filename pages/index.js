40be0005a0e149c5bf64962e3926b94308a0af87
= <=>
2022-06-15 13:41:54 +0530

    latest

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 import Banner from "../components/banner";
@@ -19,6 +20,14 @@ export default function Home() {
           buttonText="View stores nearby!"
           handleOnClick={handleOnBannerClick}
         />
+        <div className={styles.heroImage}>
+          <Image
+            src="/static/hero-image.png"
+            width={700}
+            height={400}
+            alt="hero"
+          />
+        </div>
       </main>
     </div>
   );
