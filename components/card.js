1e113596560a8a886e984fdf9464e25928964d8e
= <=>
2022-06-16 13:49:07 +0530

    latest

--- components/card.js
+++ components/card.js
@@ -1,13 +1,28 @@
 import Link from "next/link";
 import Image from "next/image";
+import cls from "classnames";
+
+import styles from "./card.module.css";
 
 const Card = (props) => {
   return (
     <div>
       <Link href={props.href}>
-        <a>
-          <h2>{props.name}</h2>
-          <Image src={props.imgUrl} width={260} height={160} alt="" />
+        <a className={styles.cardLink}>
+          <div className={cls("glass", styles.container)}>
+            <div className={styles.cardHeaderWrapper}>
+              <h2 className={styles.cardHeader}>{props.name}</h2>
+            </div>
+            <div className={styles.cardImageWrapper}>
+              <Image
+                className={styles.cardImage}
+                src={props.imgUrl}
+                width={260}
+                height={160}
+                alt=""
+              />
+            </div>
+          </div>
         </a>
       </Link>
     </div>
