110e3cfdc7d7e0789319ceab57d61cfce5814efe
= <=>
2022-06-25 14:52:03 +0530

    latest

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
+import { createContext } from "react";
 import "../styles/globals.css";
+const StoreContext = createContext();
+
+const StoreProvider = ({ children }) => {
+  const initailState = {
+    latLong: "",
+    coffeeStore: [],
+  };
+  return (
+    <StoreContext.Provider value={{ state: initailState }}>
+      {children}
+    </StoreContext.Provider>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <StoreProvider>
       <Component {...pageProps} />
-    </>
+    </StoreProvider>
   );
 }
 
