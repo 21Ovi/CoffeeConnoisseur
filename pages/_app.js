69c6e8fe2ac818d873a3da15432882d52f44e1d2
= <=>
2022-06-25 15:13:19 +0530

    latest

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,34 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 import "../styles/globals.css";
 const StoreContext = createContext();
+
+const ACTION_TYPES = {
+  SET_LAT_LONG: "SET_LAT_LONG",
+  SET_COFFEE_STORES: "SET_COFFEE_STORES",
+};
+
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return { ...state, latLong: action.payload.latLong };
+    }
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+      return { ...state, coffeeStores: action.payload.coffeeStores };
+    }
+    default:
+      throw new Error(`Unhandled action type ${action.type}`);
+  }
+};
 
 const StoreProvider = ({ children }) => {
   const initailState = {
     latLong: "",
     coffeeStore: [],
   };
+
+  const [state, dispatch] = useReducer(storeReducer, initailState);
   return (
-    <StoreContext.Provider value={{ state: initailState }}>
+    <StoreContext.Provider value={{ state, dispatch }}>
       {children}
     </StoreContext.Provider>
   );
