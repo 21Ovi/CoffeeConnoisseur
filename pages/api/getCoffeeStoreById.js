89e12b883743c6047dddb6a4eefe1001d82abe5e
= <=>
2022-06-27 23:32:56 +0530

    latest

--- pages/api/getCoffeeStoreById.js
+++ pages/api/getCoffeeStoreById.js
@@ -1,9 +1,23 @@
-const getCoffeeStoreById = (req, res) => {
+import { table, getMinifiedRecords } from "../../lib/airtable";
+
+const getCoffeeStoreById = async (req, res) => {
   const { id } = req.query;
 
   try {
     if (id) {
-      res.json({ message: `id is created ${id}` });
+      const findCoffeeStoreRecords = await table
+        .select({
+          filterByFormula: `id="${id}"`,
+        })
+        .firstPage();
+
+      if (findCoffeeStoreRecords.length !== 0) {
+        const records = getMinifiedRecords(findCoffeeStoreRecords);
+
+        res.json(records);
+      } else {
+        res.json({ message: `id could not be found` });
+      }
     } else {
       res.status(400);
       res.json({ message: "Id is Missing!" });
