f89e1c88b901831e68463759a390773a5cdde496
= <=>
2022-06-28 23:45:44 +0530

    latest

--- pages/api/favouriteCoffeeStoreById.js
+++ pages/api/favouriteCoffeeStoreById.js
@@ -1,4 +1,25 @@
-const favouriteCoffeeStoreById = (req, res) => {
-  res.json({ Message: "This works" });
+import { findRecordByFilter } from "../../lib/airtable";
+
+const favouriteCoffeeStoreById = async (req, res) => {
+  if (req.method === "PUT") {
+    try {
+      const { id } = req.body;
+
+      if (id) {
+        const records = await findRecordByFilter(id);
+        if (records.length !== 0) {
+          res.json(records);
+        } else {
+          res.json({ Message: "Coffee store id dosen't exist", id });
+        }
+      } else {
+        res.status(400);
+        res.json({ message: "Id is missing" });
+      }
+    } catch (error) {
+      res.status(500);
+      res.json({ message: "Error upvoting coffee store", error });
+    }
+  }
 };
 export default favouriteCoffeeStoreById;
